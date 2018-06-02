dd536d44d08ac215604e719a9e748028c9f7b92d
mbboyraz <mbb3458@gmail.com>
2018-06-02 18:40:44 +0300

    aranma yeri düzenlendi. tekrar

--- app/src/main/java/techheromanish/example/com/videochatapp/MainActivity.java
+++ app/src/main/java/techheromanish/example/com/videochatapp/MainActivity.java
@@ -3,7 +3,6 @@ package techheromanish.example.com.videochatapp;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.DataSetObserver;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -18,11 +17,9 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
-
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
-import techheromanish.example.com.videochatapp.Manifest.permission;
 
 public class MainActivity extends ListActivity {
 
